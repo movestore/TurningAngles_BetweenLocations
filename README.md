f94131d3cfbc9f801ef8fe1ad6a516d734874f76
annescharf <ascharf@ab.mpg.de>
2024-01-22 22:54:54 +0100

    update to move2

--- README.md
+++ README.md
@@ -10,23 +10,23 @@ Calculation of the turning angle between consecutive locations.
 ## Documentation
 This App calculates the turning angle between consecutive locations. Three locations are necessary for the calculation of each turning angle. Therefore the turning angle of the first and the last location of the track will be set to NA. 
 
-The turning angles are calculated as angles in degrees from -180 to 180.
+The turning angles are calculated as angles in radians (-pi to pi) or degrees (-180 to 180). Where negative values are left turns and positive right turns.
 
 A column named _**turnAngle**_ will be appended to the dataset that is returned for further use in next Apps.
 
 A histogram of the turning angle distribution of all individuals and per individual is automatically created and can be downloaded in the output as a pdf.
 
 ### Input data
-moveStack in Movebank format
+move2_locs
 
 ### Output data
-moveStack in Movebank format
+move2_locs
 
 ### Artefacts
 `turnAngle_histogram.pdf`: PDF with histograms of the turning angles per individual
 
 ### Settings
-no settings 
+**Units:** the units of the resulting values can be selected. Available are: `radians` (-pi to pi), `degrees` (-180 to 180). Default is `radians`
 
 ### Null or error handling
-**Data**: The full input dataset with the addition of turning angles is returned for further use in a next App and cannot be empty.
+**Data**: The full input dataset with the addition of the column _**turnAngle**_ is returned for further use in a next App and cannot be empty.
--- RFunction.R
+++ RFunction.R
@@ -1,26 +1,48 @@
-library('move')
+library('move2')
 library("ggplot2")
+library("sf")
+library("units")
 
-rFunction <-  function(data) {
-  data$turnAngle <- unlist(lapply(turnAngleGc(data), function(x) c(NA, as.vector(x), NA)))
-    
-    if(length(levels(trackId(data)))==1){
-      dataDF <- data.frame(turnAngle=data$turnAngle,indv=namesIndiv(data))
-      turnAngleHist <- ggplot(dataDF, aes(turnAngle))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Turning angle (deg)", breaks=c(-180,-90,0,90,180), labels=c("-180(S)","-90(W)","0(N)","90(E)","180(S)"), limits=c(-180,180))
-      pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "turnAngle_histogram.pdf"))
-      print(turnAngleHist)
-      dev.off()
-    } else {
+rFunction <-  function(data, setunits){
+  
+  if(!st_crs(data)[[1]]=="EPSG:4326"){
+    dataLL <- st_transform(data,crs="EPSG:4326")
+  }
+  if(st_crs(data)[[1]]=="EPSG:4326"){
+    dataLL <- data
+  }
+  
+  dataLL$turnAngle <- mt_turnangle(dataLL, units=setunits)
+  if(setunits=="rad"){
+    setbreaks <- c(-pi,-(pi/2),0,(pi/2),pi)
+    setlim <- c(-pi,pi)
+    setlabels <- round(c(-pi,-(pi/2),0,(pi/2),pi),2)
+  }
+  if(setunits=="degrees"){
+    setbreaks <- c(-180,-(180/2),0,(180/2),180)
+    setlim <- c(-180,180)
+    setlabels <- c(-180,-(180/2),0,(180/2),180)
+  }
+  
+  if(length(levels(as.factor(mt_track_id(dataLL))))==1){
+    dataDF <- data.frame(turnAngle=drop_units(dataLL$turnAngle),indv=unique(mt_track_id(dataLL)))
+    turnAngleHist <- ggplot(dataDF, aes(turnAngle))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Turning angle (rad)", breaks=setbreaks, labels=setlabels, limits=setlim)
     pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "turnAngle_histogram.pdf"))
-      turnAngleHistAll <- ggplot(data@data, aes(turnAngle))+geom_histogram(bins=100)+ ggtitle("All Individuals") +theme_bw()+ scale_x_continuous("Turning angle (deg)", breaks=c(-180,-90,0,90,180), limits=c(-180,180))
+    print(turnAngleHist)
+    dev.off()
+  }else {
+    pdf(paste0(Sys.getenv(x = "APP_ARTIFACTS_DIR", "/tmp/"), "turnAngle_histogram.pdf"))
+    turnAngleHistAll <- ggplot(dataLL, aes(drop_units(turnAngle)))+
+      geom_histogram(bins=100)+ ggtitle("All Individuals") +theme_bw()+ 
+      scale_x_continuous("Turning angle (rad)", breaks=setbreaks, labels=setlabels, limits=setlim)
     print(turnAngleHistAll)
-    lapply(split(data), function(x){
-      dataDF <- data.frame(turnAngle=x$turnAngle, indv=namesIndiv(x)) 
-      turnAngleHist <- ggplot(dataDF, aes(turnAngle))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Turning angle (deg)", breaks=c(-180,-90,0,90,180), limits=c(-180,180))
+    lapply(split(dataLL, mt_track_id(dataLL)), function(x){
+      dataDF <- data.frame(turnAngle=drop_units(x$turnAngle), indv=unique(mt_track_id(x))) 
+      turnAngleHist <- ggplot(dataDF, aes(turnAngle))+geom_histogram(bins=100)+facet_grid(~indv)+ theme_bw()+ scale_x_continuous("Turning angle (rad)", breaks=setbreaks, labels=setlabels, limits=setlim)
       print(turnAngleHist)
     })
     dev.off() 
-    
   }
+  data$turnAngle <- dataLL$turnAngle
   return(data)
 }
